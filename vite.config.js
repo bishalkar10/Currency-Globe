2bade916822fe407e4300b69baffcf3a28016052
Bishal Kar <bishalkar10@gmail.com>
2023-09-14 04:31:51 +0530

    Configured env variable and base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = dotenv.config({
+    path: `.env.${mode}`,
+  }).parsed;
+
+  // Pass the environment variables to Vite's define plugin
+  return {
+    base: "/",
+    plugins: [react()],
+    define: {
+      "process.env": env,
+    },
+  };
 });
