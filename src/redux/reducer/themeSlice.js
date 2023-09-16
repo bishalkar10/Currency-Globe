18de8874007fae4fe275a56e04de2b5f2c82cc1b
Bishal Kar <bishalkar10@gmail.com>
2023-09-16 17:02:38 +0530

    Changed the initial state from a string hex code to a object of hex values

--- src/redux/reducer/themeSlice.js
+++ src/redux/reducer/themeSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const themeSlice = createSlice({
   name: "theme",
   initialState: {
-    color: "#1E40AF", // Set an initial color
+    color: { hover: "#dc2626", main: "#b91c1c", active: "#991b1b" }, // red
   },
   reducers: {
     changeColor: (state, action) => {
